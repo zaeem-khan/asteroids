ef7e9567149077e24b9a8d11fd1bc63b0c753a67
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-05-05 11:51:27 -0500

    asteroid setup

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    af = AsteroidField()
+
     dt = 0
     while True:
         for event in pygame.event.get():
