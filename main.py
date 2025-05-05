6c155f836e8dc6903d0120d559a846183e729eae
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-05-05 11:15:11 -0500

    rotate with a and d keys

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -17,3 +17,14 @@ class Player(CircleShape):
     def draw(self, screen):
         points = self.triangle()
         pygame.draw.polygon(screen, "white", points, 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
