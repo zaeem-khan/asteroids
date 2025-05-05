fd66eba8444bf7c3e5cde285964b9c1654abe147
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-05-05 11:41:08 -0500

    Use groups instead of calling classes directly

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
