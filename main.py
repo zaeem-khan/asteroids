ffe287e13c54c475f9ecfe75724e38d15ce73d4f
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-05-05 11:56:32 -0500

    collison detection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import *
@@ -28,6 +29,11 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
         
         screen.fill("black")
         
