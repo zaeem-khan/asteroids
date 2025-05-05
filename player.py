a5968ad93f25ced0fed23a5a1173fdb1a80f3959
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-05-05 11:09:36 -0500

    generate player sprite

---
+++ player.py
@@ -0,0 +1,19 @@
+from constants import *
+from circleshape import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen, "white", points, 2)
